61909b9e01fb94697a750119504afbd0d9650080
Christopher Sefcik <cdsefcik@gmail.com>
2021-12-11 17:33:13 -0500

    Finish second plots

--- Artificial_Intelligence/Seaborn.py
+++ Artificial_Intelligence/Seaborn.py
@@ -1,25 +1,25 @@
 import seaborn as sns
-
+import numpy as np
 
 
 #1 Distribution plots
 
-#Load dataset
+#Load dataset.
 tips = sns.load_dataset('tips')
 
-#Plot a histogram in a dataset
+#Plot a histogram in a dataset.
 sns.distplot(tips['total_bill'], kde = False,bins = 30 )
 
 
-#Combined multiple distibutions
+#Combined multiple distibutions.
 sns.jointplot(x='total_bill',y='tip', data=tips)
 
 
-#Combined multiple distibutions hexagon distribution
+#Combined multiple distibutions hexagon distribution.
 sns.jointplot(x='total_bill',y='tip', data=tips, kind = 'hex')
 
 
-#Combined multiple distibutions shows a linear fit
+#Combined multiple distibutions shows a linear fit.
 sns.jointplot(x='total_bill',y='tip', data=tips, kind = 'reg')
 
 
@@ -29,7 +29,25 @@ sns.pairplot(tips, hue='sex', palette = 'blue')
 
 sns.rugplot(tips['total_bill'])
 
+#Bar plots.
+sns.barplot(x='sex',y='total_bill',data=tips, estimator=np.std)
 
+#Count plot.
+sns.countplot(x='sex', data=tips)
 
+#2 Catogorical plots
 
+#Box plots, dots are outliers, hue separates data based on a categorical data point.
+sns.boxplot(x='day', y='total_bill',data=tips, hue='smoker')
 
+#Violin Plot.
+sns.violinplot(x='day', y='total_bill',data=tips, hue='sex')
+
+#Strip plot.
+sns.stripplot(x='day',y='total_bill',data=tips, jitter=True)
+
+#Swarmplot.
+sns.swarmplot(x='day',y='total_bill',data=tips)
+
+#Factor plot, can call in any plot based on the kind parameter.
+sns.factorplot(x='day',y='total_bill',data=tips, kind='bar')
